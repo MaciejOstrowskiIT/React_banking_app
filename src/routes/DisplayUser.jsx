1c7fa59fcab2c780139ec67476a6681ea81863ef
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2022-04-03 12:39:40 +0200

    minor changes

--- src/routes/DisplayUser.jsx
+++ src/routes/DisplayUser.jsx
@@ -3,11 +3,9 @@ import { UserContext } from '../context/UserContextProvider';
 import React from 'react';
 
 export const DisplayUser = () => {
-    const userContextValue = useContext(UserContext);
-
     return (
         <div>
-            <p>{userContextValue.username}</p>
+            <p>Display User</p>
         </div>
     );
 };
