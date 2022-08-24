0e3c4b3fc76252f675d0f78563e9d022b8785cf5
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2022-08-24 22:38:42 +0200

    useLogin changes + code minor changes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,7 +5,7 @@ import useContextUpdate from '../../hooks/useContextUpdate';
 import { UserContext } from '../../context/UserContextProvider';
 
 export const Menu = () => {
-    const [userContextValue] = useContextUpdate();
+    const [] = useContextUpdate();
 
     return (
         <>
@@ -14,111 +14,129 @@ export const Menu = () => {
                     <h1 className="app-name-header">
                         React Banking App
                     </h1>
-                    <p className="logged-as-paragraph">
-                        {userContextValue.contextIsLoggedIn
-                            ? `Zalogowano jako: ${userContextValue.contextUsername}`
-                            : 'Użytkownik niezalogowany'}
-                    </p>
                 </header>
 
                 <UserContext.Consumer>
                     {(context) => (
-                        <nav
-                            className="navbar"
-                            style={{
-                                borderBottom: 'solid 1px',
-                                paddingBottom: '1rem',
-                            }}>
-                            {!context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/login"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Zaloguj się
-                                </NavLink>
-                            )}
-                            {!context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/register"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Zarejestruj się
-                                </NavLink>
-                            )}
-                            {context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/main"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Strona główna
-                                </NavLink>
-                            )}
-                            {context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/transfers"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Przelewy
-                                </NavLink>
-                            )}
-                            {context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/cards"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Karty
-                                </NavLink>
-                            )}
-                            {context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/help"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Pomoc
-                                </NavLink>
-                            )}
-                            {context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/settings"
-                                    className={(navData) =>
-                                        navData.isActive
-                                            ? 'active-link'
-                                            : ''
-                                    }>
-                                    Ustawienia
-                                </NavLink>
-                            )}
-                            {context.contextIsLoggedIn && (
-                                <NavLink
-                                    to="/"
-                                    onClick={() => {
-                                        localStorage.removeItem(
-                                            'token'
-                                        );
-                                        context.contextIsLoggedIn = false;
-                                    }}>
-                                    Wyloguj
-                                </NavLink>
-                            )}
-                        </nav>
+                        <>
+                            <p className="logged-as-paragraph">
+                                {context.contextIsLoggedIn
+                                    ? `Zalogowano jako: ${context.contextUsername}`
+                                    : 'Użytkownik niezalogowany'}
+                            </p>
+                            <nav
+                                className="navbar"
+                                style={{
+                                    borderBottom:
+                                        'solid 1px',
+                                    paddingBottom: '1rem',
+                                }}>
+                                {!context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/login"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Zaloguj się
+                                    </NavLink>
+                                )}
+                                {!context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/register"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Zarejestruj się
+                                    </NavLink>
+                                )}
+                                {context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/main"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Strona główna
+                                    </NavLink>
+                                )}
+                                {context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/transfers"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Przelewy
+                                    </NavLink>
+                                )}
+                                {context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/cards"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Karty
+                                    </NavLink>
+                                )}
+                                {context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/help"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Pomoc
+                                    </NavLink>
+                                )}
+                                {context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/settings"
+                                        className={(
+                                            navData
+                                        ) =>
+                                            navData.isActive
+                                                ? 'active-link'
+                                                : ''
+                                        }>
+                                        Ustawienia
+                                    </NavLink>
+                                )}
+                                {context.contextIsLoggedIn && (
+                                    <NavLink
+                                        to="/"
+                                        onClick={() => {
+                                            localStorage.removeItem(
+                                                'token'
+                                            );
+                                            context.contextIsLoggedIn = false;
+                                            window.location.reload();
+                                        }}>
+                                        Wyloguj
+                                    </NavLink>
+                                )}
+                            </nav>
+                        </>
                     )}
                 </UserContext.Consumer>
                 <Outlet />
--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-let navigate = useNavigate();
+const useLogin = () => {
+    let navigate = useNavigate();
 
-useEffect(() => {
-    const token = localStorage.getItem('token');
+    useEffect(() => {
+        const token = localStorage.getItem('token');
 
-    if (token) {
-        const user = jsonwebtoken.decode(token);
-        console.log(user);
-        if (!user) {
-            localStorage.removeItem('token');
-            navigate('/login');
-        } else {
-            navigate('/main');
+        if (token) {
+            const user = jsonwebtoken.decode(token);
+            if (!user) {
+                localStorage.removeItem('token');
+                navigate('/login');
+            } else {
+                navigate('/main');
+            }
         }
-    }
-}, []);
+    }, []);
+};
+
+export default useLogin;
