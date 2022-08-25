6e12e81e4d3e8ebd11cc1e313a06dfb7d6abf45a
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2022-08-25 13:57:37 +0200

    quote component changes

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,65 +1,22 @@
-import jsonwebtoken from 'jsonwebtoken';
-import React, {
-    useContext,
-    useEffect,
-    useState,
-} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
 import setQuoteInDB from '../../api/setQuoteInDB';
 import { UserContext } from '../../context/UserContextProvider';
+import CheckToken from '../../functions/CheckToken';
 
 export const Quote = () => {
     const userContextValue = useContext(UserContext);
-
-    const serverIPAddress =
-        process.env
-            .REACT_APP_MIDDLEWARE_SERVER_IP_ADDRESS ||
-        '192.168.1.9';
-    const serverPort =
-        process.env.REACT_APP_MIDDLEWARE_SERVER_PORT ||
-        '192.168.1.9';
-
-    const [quote, setQuote] = useState('');
     const [tempQuote, setTempQuote] = useState('');
-    let navigate = useNavigate();
 
-    async function populateQuote() {
-        const req = await fetch(
-            `http://${serverIPAddress}:${serverPort}/api/quote`,
-            {
-                headers: {
-                    'x-access-token':
-                        localStorage.getItem('token'),
-                },
-            }
-        );
-        const data = await req.json();
-        if (data.status === 'ok') {
-            setQuote(data.quote);
-        } else {
-            console.log('data error');
-        }
-    }
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-
-        if (token) {
-            const user = jsonwebtoken.decode(token);
-            if (!user) {
-                localStorage.removeItem('token');
-                navigate('/login');
-            } else {
-                populateQuote();
-            }
-        } else {
-            navigate('/login');
-        }
-    }, []);
+    CheckToken();
 
     return (
         <>
-            <h2>Twój status: {quote}</h2>
+            <h2>
+                Twój status:{' '}
+                <UserContext.Consumer>
+                    {(context) => context.contextQuote}
+                </UserContext.Consumer>
+            </h2>
             <form onSubmit={() => setQuoteInDB(tempQuote)}>
                 <input
                     type="text"
